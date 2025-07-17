e76cb4c35eac5e808b1982c8fc897fd46865ab37
yztzy <157469291@qq.com>
2025-07-17 20:36:13 +0800

    feat(group): 添加群组搜索功能并优化网关过滤逻辑
    - 在 GroupController 中添加 searchGroup 方法，实现群组搜索功能
    - 更新 TokenAuthFilter 中的过滤逻辑，移除了无用的判断语句

--- studyParty_group/src/main/java/com/studyparty/group/controller/GroupController.java
+++ studyParty_group/src/main/java/com/studyparty/group/controller/GroupController.java
@@ -1,8 +1,36 @@
 package com.studyparty.group.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.studyparty.group.common.Result;
+import com.studyparty.group.domian.Group;
+import com.studyparty.group.mapper.GroupMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GroupController {
+    @Autowired
+    private GroupMapper groupMapper;
+    @PostMapping("/searchGroup")
+    public Result<?> searchGroup(String searchContext, int currentPage,boolean canJoin) {
+        if (currentPage <= 0){
+            return Result.error("请输入正确的页数");
+        }
+        if (searchContext.trim().isEmpty()){
+            return Result.error("请输入正确的搜索内容");
+        }
+        Page<Group> page = new Page<>(currentPage, 10);
+        LambdaQueryWrapper<Group> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(Group::getGroupName, searchContext.trim());
+        queryWrapper.or().like(Group::getSlogan, searchContext.trim());
+        if (canJoin){
+            queryWrapper.apply("people_num<max_people_unm");
+        }
+        // 可以根据需要设置查询条件
+        return Result.success(groupMapper.selectPage(page, queryWrapper));
+    }
 
 }
